64431f545f981ef57eb7759f86106296fe11498b
MahmoudReza Izadiyar <64728244+mahmoudreza-izadiyar@users.noreply.github.com>
2024-06-22 12:48:34 +1000

    create modules and controller

--- schedule-management-api/src/app.module.ts
+++ schedule-management-api/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SchedulesModule } from './schedules/schedules.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
+  imports: [SchedulesModule, TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
