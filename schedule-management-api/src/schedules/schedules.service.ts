e6f3cab76e958545756abd5928b20605f4e6eecd
MahmoudReza Izadiyar <64728244+mahmoudreza-izadiyar@users.noreply.github.com>
2024-06-22 13:24:06 +1000

    Implement Schedules Service and Controller

--- schedule-management-api/src/schedules/schedules.service.ts
+++ schedule-management-api/src/schedules/schedules.service.ts
@@ -1,4 +1,56 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import { Schedule, Prisma } from '@prisma/client';
 
 @Injectable()
-export class SchedulesService {}
+export class SchedulesService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(data: Prisma.ScheduleCreateInput): Promise<Schedule> {
+    try {
+      return await this.prisma.schedule.create({ data });
+    } catch (error) {
+      console.error('Error in create method:', error);
+      throw error;
+    }
+  }
+
+  async findAll(): Promise<Schedule[]> {
+    try {
+      return await this.prisma.schedule.findMany();
+    } catch (error) {
+      console.error('Error in findAll method:', error);
+      throw error;
+    }
+  }
+
+  async findOne(id: string): Promise<Schedule | null> {
+    try {
+      return await this.prisma.schedule.findUnique({ where: { id } });
+    } catch (error) {
+      console.error('Error in findOne method:', error);
+      throw error;
+    }
+  }
+
+  async update(
+    id: string,
+    data: Prisma.ScheduleUpdateInput,
+  ): Promise<Schedule> {
+    try {
+      return await this.prisma.schedule.update({ where: { id }, data });
+    } catch (error) {
+      console.error('Error in update method:', error);
+      throw error;
+    }
+  }
+
+  async remove(id: string): Promise<Schedule> {
+    try {
+      return await this.prisma.schedule.delete({ where: { id } });
+    } catch (error) {
+      console.error('Error in remove method:', error);
+      throw error;
+    }
+  }
+}
